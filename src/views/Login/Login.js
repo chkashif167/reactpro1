65cf8b975721d5680bfbb3d61cd5867b3dfad9b1
kashifdeltashoppe167 <kashif@deltahsoppe.com>
2019-12-11 23:16:36 +0500

    register submitted with fake api using axios

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -8,101 +8,42 @@ import {
   handleBlur,
   handleChange
 } from "formik";
+import { useFormik } from "formik";
 import * as Yup from "yup";
 import { Button, FormGroup, Label, Input, Container, Col } from "reactstrap";
 import "./Login.scss";
 import userRegister from "../../store/reducers/registerReducer";
 import { addUserAction } from "../../store/reducers/registerReducer.js";
 import InputField from "../../components/FormFields/InputField.js";
-const Login = () => {
-  //const [user, setUser] = useState("");
-  //const usedispatch = useDispatch();
-  const [user, setUser] = useState("");
-  const usedispatch = useDispatch();
-  const addUser = user => usedispatch(addUserAction(user));
 
-  const ShowUser = useSelector(state => state.userRegister.fullname);
+const Login = props => {
+  const { handleBlur, handleChange, values, errors, touched } = props;
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: ""
+    },
+
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    }
+  });
 
   return (
-    <div className="login_container">
-      <Formik
-        initialValues={{
-          email: "",
-          password: ""
-        }}
-        validationSchema={Yup.object().shape({
-          email: Yup.string()
-            .email("Email is invalid")
-            .required("Email is required"),
-          password: Yup.string().required("Password is required")
-        })}
-        onSubmit={fields => {
-          addUser(fields);
-          alert("SUCCESS!! :-)\n\n" + JSON.stringify(fields, null, 4));
-        }}
-        render={props => {
-          const {
-            handleBlur,
-            handleChange,
-            values,
-            errors,
-            status,
-            touched
-          } = props;
-          return (
-            <Form className="register_form">
-              <h1> Login</h1>
-              {console.log(errors)}
-
-              <InputField
-                htmlFor="email"
-                label="Email"
-                type="email"
-                name="email"
-                className={
-                  "form-control" +
-                  (errors.email && touched.email ? " is-invalid" : "")
-                }
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.email}
-                errorLabel={errors.email}
-                errorClassname="invalid-feedback"
-                iconClass=" email"
-                placeholder="Email"
-              />
-
-              <InputField
-                htmlFor="password"
-                label="Password"
-                type="password"
-                name="password"
-                className={
-                  "form-control" +
-                  (errors.password && touched.password ? " is-invalid" : "")
-                }
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.password}
-                errorLabel={errors.password}
-                errorClassname="invalid-feedback"
-                iconClass=" password"
-                placeholder="Password"
-              />
-
-              <div className="form-group">
-                <button type="submit" className="btn btn-primary mr-2">
-                  Login
-                </button>
-                <button type="reset" className="btn btn-secondary">
-                  Register
-                </button>
-              </div>
-            </Form>
-          );
-        }}
+    <form onSubmit={formik.handleSubmit}>
+      <InputField
+        label="email"
+        name="email"
+        className={errors.email && touched.email && " is-invalid"}
+        onBlur={handleBlur}
+        onChange={handleChange}
+        value={formik.values.email}
+        errorLabel={errors.email}
+        iconClass="email"
+        placeholder="Email "
       />
-    </div>
+      <button type="submit">Submit</button>
+    </form>
   );
 };
 
